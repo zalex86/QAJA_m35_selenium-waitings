a29923d1628302dc532ed3e7138063ebd8d3e7d4
SkillfactoryCoding <65289009+SkillfactoryCoding@users.noreply.github.com>
2021-09-03 13:01:42 +0300

    Add files via upload

---
+++ src/test/java/ru/rockstarhamster/selenium/waitings/TestTest.java
@@ -0,0 +1,22 @@
+package ru.rockstarhamster.selenium.waitings;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class TestTest {
+    @Test
+    public void open_google() {
+        System.setProperty("webdriver.chrome.driver", "/home/alex/IdeaProjects/selenium-watings/src/test/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("some id")));
+        driver.get("https://google.com");
+    }
+}
